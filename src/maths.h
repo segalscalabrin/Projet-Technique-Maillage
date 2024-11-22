c01c3ad002feb607a17a746c9501ba029b8de633
Elise_Clavel <152402398+EliseClavel@users.noreply.github.com>
2024-11-22 09:28:45 +0100

    Add files via upload

---
+++ src/maths.h
@@ -0,0 +1,5 @@
+#include "include.h"
+
+void calculerCercleCirconscrit(Triangle *t);
+bool pointDansCercleCirconscrit(Triangle *triangle, Point *point);
+Cavite idCavite (vector<Triangle> *triangulation, Point *pt);
