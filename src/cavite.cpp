679d6386d2033e5b8f33406fbe2be07e7f1aa3e5
Laptop Segal Raguenes-Scalabrin <segal.scalabrin@gmail.com>
2024-11-22 11:40:56 +0100

    cav modif

--- src/cavite.cpp
+++ src/cavite.cpp
@@ -1,13 +1,13 @@
 #include "cavite.h"
 
 
+
 Cavite idCavite (vector<Triangle> *triangulation, Point *pt)
 {
     Cavite cavite;
     // pour tous les triangles de la triangulation
     for (unsigned int t=0; t<triangulation->size(); t++)
     {
-        //Triangle tri = triangulation->at(t);
         // si le triangle est valide/existe
         if(triangulation->at(t).valide)
         {
@@ -33,6 +33,7 @@ Cavite idCavite (vector<Triangle> *triangulation, Point *pt)
                             isSommetInCavite = true;
                         }
                     }
+                    // si le sommet n'est pas présent dans la cavite, on le rajoute
                     if (!isSommetInCavite)
                     {
                         //printf("hi\n");
@@ -43,50 +44,34 @@ Cavite idCavite (vector<Triangle> *triangulation, Point *pt)
                     
                     
                     bool isArreteInCavite(false);
-
-                    //std::cout << isArreteInCavite << " " << true << std::endl;
-
                     int areteId(-1);
-                    Arete currentEdge = {-1, -1, true};
-                    //printf("hi i=%d i+1=%d triangulation->at(t).edge= %d\n", i, (i+1)%3, currentEdge.pt1);
+                    Arete currentEdge;
                     currentEdge.pt1 = triangulation->at(t).sommetID[i];
                     currentEdge.pt2 = triangulation->at(t).sommetID[(i+1)%3];
                     currentEdge.areteValide = true;
-                    std::cout << "arete valide : " << " " << currentEdge.areteValide << std::endl;
-                    printf("hi\n");
+                    
                     for (unsigned int areteC=0; areteC<cavite.aretes.size(); areteC++)
                     {
-                        printf("arete cavite : [%d, %d] arete actuelle : [%d,%d] presente : %B\n", cavite.aretes[areteC].pt1, 
-                        cavite.aretes[areteC].pt2, currentEdge.pt1, currentEdge.pt2, ((cavite.aretes[areteC].pt1 == currentEdge.pt1)&&(cavite.aretes[areteC].pt2 == currentEdge.pt2))
-                         || ((cavite.aretes[areteC].pt1 == currentEdge.pt2)&&(cavite.aretes[areteC].pt2 == currentEdge.pt1)));
-                        if (((cavite.aretes[areteC].pt1 == currentEdge.pt1)&&(cavite.aretes[areteC].pt2 == currentEdge.pt2))
+                       if (((cavite.aretes[areteC].pt1 == currentEdge.pt1)&&(cavite.aretes[areteC].pt2 == currentEdge.pt2))
                          || ((cavite.aretes[areteC].pt1 == currentEdge.pt2)&&(cavite.aretes[areteC].pt2 == currentEdge.pt1)))
                         {
-                            std::cout << "arete valide : " << " " << cavite.aretes[areteId].areteValide << std::endl;
-                            if (cavite.aretes[areteId].areteValide)
-                            {
-                                printf("hi");
-                                isArreteInCavite = true;
-                                areteId = areteC;
-                            }
-                            
+                            isArreteInCavite = true;
+                            areteId = areteC;                            
                         }
                     }
                     
-                    //printf("hi\n");
-                    std::cout << isArreteInCavite << " " << true << std::endl;
                     // si l'arrete n'est pas dans la cavité, on l'ajoute
                     if (!isArreteInCavite)
                     {
-                        //printf("hi");
                         cavite.aretes.push_back(currentEdge);
                     }
                     // on la supprime si elle y est
                     else
                     {
-                        //cavite.aretes[areteId].areteValide = false;
+                        cavite.aretes[areteId].areteValide = false;
                     }
                 }
+                printf("\n");
             }
         }
     }
